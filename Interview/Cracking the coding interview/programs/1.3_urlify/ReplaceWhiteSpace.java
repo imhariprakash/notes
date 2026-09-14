e2bec3977964becf171a1d95b791d8a5e735a094
imhariprakash <58242393+imhariprakash@users.noreply.github.com>
2026-09-14 14:43:21 +0530

    Create ReplaceWhiteSpace.java

---
+++ Interview/Cracking the coding interview/programs/1.3_urlify/ReplaceWhiteSpace.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+ 
+class ReplaceWhiteSpace{
+    public static void main(String[] args)
+    {
+        char[] str = {'M', 'r', ' ', 'J', 'o', 'h', 'n', ' ', 'S', 'm', 'i', 't', 'h', ' ', ' ', ' ', ' '};
+        int len = 13;
+        int index = str.length - 1;
+        
+        for(int i = len - 1; i >= 0; i--)
+        {
+            if(str[i] == ' ')
+            {
+                replaceWhiteSpace(str, index);
+                index = index - 3;
+            }
+            else
+            {
+                str[index] = str[i];
+                --index;
+            }
+        }
+        for(int i = 0; i < str.length; i++)
+        {
+            System.out.print(str[i]);
+        }
+    }
+
+    private static void replaceWhiteSpace(char[] str, int index)
+    {
+        str[index] = '0';
+        str[index - 1] = '2';
+        str[index - 2] = '%';
+    }
+}
